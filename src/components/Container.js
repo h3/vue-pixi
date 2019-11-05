0ba06efa7fc999d176621e336b7528bae59a5f0c
Nejc Jelovčan <nejc.jelovcan@gmail.com>
2019-11-05 09:54:04 +0100

    Container: added sortableChildren (needed for zIndex)

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,8 @@ export default {
   props: {
     height: Number,
     width: Number,
-    interactiveChildren: Boolean
+    interactiveChildren: Boolean,
+    sortableChildren: Boolean
   },
   computed: {
     instance: () => new Container()
@@ -17,12 +18,14 @@ export default {
         if (this.width) instance.width = this.width
         if (this.height) instance.height = this.height
         if (this.interactiveChildren) instance.interactiveChildren = this.interactiveChildren
+        if (this.sortableChildren) instance.sortableChildren = this.sortableChildren
       },
       immediate: true
     },
     "width": function (width) { this.instance.width = width },
     "height": function (height) { this.instance.height = height },
-    "interactiveChildren": function (interactiveChildren) { this.instance.interactiveChildren = interactiveChildren }
+    "interactiveChildren": function (interactiveChildren) { this.instance.interactiveChildren = interactiveChildren },
+    "sortableChildren": function (sortableChildren) { this.instance.sortableChildren = sortableChildren },
   },
   render (h) { return this.$slots.default ? h("div", this.$slots.default) : undefined }
 }
